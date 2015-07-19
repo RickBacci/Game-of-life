06941c3cbc8a38f4058294c15f48756946ac3b7d
Rick Bacci <rickbacci@gmail.com>
2015-07-19 02:36:26 -0600

    refactoring

--- game_of_life.rb
+++ game_of_life.rb
@@ -4,31 +4,21 @@ require './lib/cell'
 require './lib/fate'
 
 def setup
+  @board       = Board.generate_cells(starting_board, 50)
+  cell_size    = Board.cell_size
+  array_width  = Board.width
+  width        = cell_size * array_width
+  height       = cell_size * array_width
   frame_rate 2
-  @board = Board.generate_cells(starting_board, 50)
-  @cell_size = Board.cell_size
-  @array_width = Board.width
-  width  = @cell_size * @array_width
-  height = @cell_size * @array_width
-  size width, height
   smooth
-  background 0, 0, 0
+  size width, height
 end
 
 def draw
   @board.each do |cell|
-    if cell.alive?(cell)
-      fill 256, 0, 0
-      rect cell.x * 50, cell.y * 50, cell.size, cell.size
-    else
-       fill 0, 0, 0
-       rect cell.x * 50, cell.y * 50, cell.size, cell.size
-    end
-  end
-   @board.each do |cell|
-     decide_fate(cell)
+    change_colors(cell)
+    decide_fate(cell)
   end
   tick_tock
 end
-
 
